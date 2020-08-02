8b44db6b8d603ead55d2ad3fb0e0f9112067d8d4
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-02 18:05:47 +0100

    microfront end sample two

---
+++ angular-app-two/src/main/java/com/mailshine/microfrontend/angular/two/angularapptwo/AngularAppTwoApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.microfrontend.angular.two.angularapptwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAppTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAppTwoApplication.class, args);
+	}
+
+}
