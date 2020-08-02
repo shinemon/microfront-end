161ae44a2dc70b82ca63832d8c9a4184374e615e
Shinemon Divakaran <58699255+shinemon@users.noreply.github.com>
2020-08-02 13:22:55 +0100

    microfront end sample one

---
+++ angular-app-one/src/main/java/com/mailshine/microfrontend/angular/AngularAppOneApplication.java
@@ -0,0 +1,13 @@
+package com.mailshine.microfrontend.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAppOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAppOneApplication.class, args);
+	}
+
+}
